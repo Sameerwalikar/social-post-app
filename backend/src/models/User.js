4570a4227ec6d48db19661fe2b5de70948dd2cc3
Sameer <sameenaraj01@gmail.com>
2026-06-02 23:16:39 +0530

    Fix password hashing middleware for Mongoose 9

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -31,10 +31,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre("save", async function hashPassword(next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function hashPassword() {
+  if (!this.isModified("password")) return;
+
   this.password = await bcrypt.hash(this.password, 12);
-  return next();
 });
 
 userSchema.methods.comparePassword = async function comparePassword(password) {
